411536c553adf80c6b8434536763fdda7509e101
wentao <wt034038@126.com>
2018-04-05 17:22:01 +0800

    Mode1

---
+++ Example/Pods/Target Support Files/Mode1/Mode1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Mode1 : NSObject
+@end
+@implementation PodsDummy_Mode1
+@end
---
+++ Example/Pods/Target Support Files/Pods-Mode1_Tests/Pods-Mode1_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Mode1_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Mode1_Tests
+@end
